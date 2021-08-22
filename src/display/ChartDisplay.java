d09eb2462a715a9f0f221767703561827c9b5a10
XcSteven <73635652+XcSteven@users.noreply.github.com>
2021-08-22 14:28:11 +0700

    Implement ChartDisplay behavior

--- src/display/ChartDisplay.java
+++ src/display/ChartDisplay.java
@@ -4,7 +4,7 @@ import main.Summary;
 
 import java.util.ArrayList;
 
-public class ChartDisplay extends DisplayData{
+public class ChartDisplay extends DisplayData {
     //Constructor
     protected ChartDisplay(ArrayList<Summary> data) {
         super(data);
@@ -12,8 +12,71 @@ public class ChartDisplay extends DisplayData{
     }
 
     //Getter and setter
-    private void setRow(){}
+    private void setRow() {
+    }
 
     //Method
-    public void display(){}
+    private String[][] chart = new String[24][80];
+    private ArrayList<Integer> values = new ArrayList<Integer>();
+
+    public void display() {
+        setValue();
+        int size = values.size();
+        if (size >= 80) {
+            System.out.println ("ERROR: The maximum numbers of groups allowed is 79!");
+            System.exit(2);
+        }
+        else {
+            for (int i = 0; i < chart.length; i++) {
+                for (int j = 0; j < chart[0].length; j++) {
+                    chart[i][j] = " ";
+                }
+            }
+            for (int i = 0; i < chart[0].length; i++) {
+                chart[chart.length-1][i] = "_";
+            }
+            for (int i = 0; i < chart.length; i++) {
+                chart[i][0] ="|";
+            }
+            setUp();
+
+            for (int i = 0; i < chart.length; i++) {
+                for (int j = 0; j < chart[0].length; j++) {
+                    System.out.print (chart[i][j]);
+                }
+                System.out.println("");
+            }
+        }
+    }
+
+    public void setValue() {
+        ArrayList<Summary> sum = getData();
+        for (int i = 0; i < sum.size(); i++) {
+            values.add( sum.get(i).getValue());
+        }
+    }
+
+    public void setUp() {
+        int max = findMax();
+        int time = chart[0].length/values.size();
+        int count = 0;
+        int col = 1;
+        for (int i = 0; i< values.size(); i++) {
+            int row = (int) (values.get(count) * 23 / max + 0.5);
+            int rowAct = Math.abs(row - 23);
+            chart[rowAct][col] = "*";
+            count++;
+            col += time;
+        }
+    }
+
+    public int findMax() {
+        int max = values.get(0);
+        for (int i = 0; i < values.size(); i++) {
+            if (max < values.get(i)) {
+                max = values.get(i);
+            }
+        }
+        return max;
+    }
 }
